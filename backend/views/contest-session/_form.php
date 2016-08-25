da1ad4a5f711cee1efb613d69a5cf259d03b495c
Lac <lac.phan@enpii.com>
2016-08-25 11:41:11 +0700

    hide user id

--- backend/views/contest-session/_form.php
+++ backend/views/contest-session/_form.php
@@ -27,8 +27,6 @@ use backend\models\ContestItem;
                     <div class="contest-session-form">
                         <?php $form = ActiveForm::begin(['options' => ['class' => 'form-body', 'enctype' => 'multipart/form-data']]); ?>
 
-                        <?= $form->field($model, 'user_id')->textInput() ?>
-
                         <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
 
                         <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
@@ -92,7 +90,7 @@ use backend\models\ContestItem;
                         </div>
                         <?= $form->field($model, 'attachment_id')->hiddenInput(['id' => 'attachment-id'])->label(false) ?>
                         <?php ActiveForm::end(); ?>
-                        
+
                     </div>
                     <!-- END FORM-->
                 </div>
